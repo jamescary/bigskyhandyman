719fcf92b7f88f839d43b9d0a5aa28f3bcb91d75
James Cary <eliteeggnog@gmail.com>
2021-02-24 14:35:09 -0600

    correct strokeWidth attr

--- components/hero.js
+++ components/hero.js
@@ -48,7 +48,7 @@ export default function Hero() {
                         <path
                           stroke-linecap="round"
                           stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeWidth="2"
                           d="M4 6h16M4 12h16M4 18h16"
                         />
                       </svg>
@@ -133,7 +133,7 @@ export default function Hero() {
                       <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeWidth="2"
                         d="M6 18L18 6M6 6l12 12"
                       />
                     </svg>
